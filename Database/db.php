ab4245fd724adae1add96e5038ef40fb68c782e8
smoncavage <moncavage@hotmail.com>
2021-10-22 17:03:31 -0400

    remove aws key from db file

--- Database/db.php
+++ Database/db.php
@@ -2,7 +2,6 @@
 class Database{
  
     // specify your own database credentials
-	private string $connectionstring = "mysql://uspyiewwt6a47kiq:fhjf5mht8clgqbs8@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kirnyw4ar361d8qd";
     private string $host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     private string $db_name = "kirnyw4ar361d8qd";
     private string $username = "uspyiewwt6a47kiq";
